01dc0d2ac3c0c1745c9b78fec1e7cd88c6364a52
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-01-15 17:34:00 +0100

    updated version

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='uiform',
-    version='0.0.5',
+    version='0.0.6',
     author='UiForm',
     author_email='contact@uiform.com',
     description='UiForm official python library',
