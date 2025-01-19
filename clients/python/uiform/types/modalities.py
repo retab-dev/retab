59514fea55f607e4a3609e7e679576c2d5d746f5
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-01-19 01:12:07 +0100

    update modalities

--- clients/python/uiform/types/modalities.py
+++ clients/python/uiform/types/modalities.py
@@ -3,10 +3,11 @@ from typing import Literal
 
 BaseModality = Literal["text", "image", "audio", "video"]
 Modality = Literal[BaseModality, "native"]
-TYPE_FAMILIES = Literal["excel", "word", "powerpoint", "pdf", "image", "text", "email", "audio"]
+TYPE_FAMILIES = Literal["excel", "word", "powerpoint", "pdf", "image", "text", "email", "audio", "html"]
 NativeModalities: dict[TYPE_FAMILIES, Modality] = {
     'excel': 'image',
     'word': 'image',
+    'html': 'text',
     'powerpoint': 'image',
     'pdf': 'image',
     'image': 'image',
@@ -20,7 +21,8 @@ WORD_TYPES = Literal[".doc", ".docx", ".odt"]
 PPT_TYPES = Literal[".ppt", ".pptx", ".odp"]
 PDF_TYPES = Literal[".pdf"]
 IMAGE_TYPES = Literal[".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"]
-TEXT_TYPES = Literal[".txt", ".csv", ".tsv", ".md", ".log", ".html", ".htm", ".xml", ".json", ".yaml", ".yml", ".rtf", ".ini", ".conf", ".cfg", ".nfo", ".srt", ".sql", ".sh", ".bat", ".ps1", ".js", ".jsx", ".ts", ".tsx", ".py", ".java", ".c", ".cpp", ".cs", ".rb", ".php", ".swift", ".kt", ".go", ".rs", ".pl", ".r", ".m", ".scala"]
-EMAIL_TYPES = Literal[".eml", ".msg"]
+TEXT_TYPES = Literal[".txt", ".csv", ".tsv", ".md", ".log", ".xml", ".json", ".yaml", ".yml", ".rtf", ".ini", ".conf", ".cfg", ".nfo", ".srt", ".sql", ".sh", ".bat", ".ps1", ".js", ".jsx", ".ts", ".tsx", ".py", ".java", ".c", ".cpp", ".cs", ".rb", ".php", ".swift", ".kt", ".go", ".rs", ".pl", ".r", ".m", ".scala"]
+HTML_TYPES = Literal[".html", ".htm"]
+EMAIL_TYPES = Literal[".eml", ".msg", ".mhtml"]
 AUDIO_TYPES = Literal[".mp3", ".mp4", ".mpeg", ".mpga", ".m4a", ".wav", ".webm"]
-SUPPORTED_TYPES = Literal[EXCEL_TYPES, WORD_TYPES, PPT_TYPES, PDF_TYPES, IMAGE_TYPES, TEXT_TYPES, EMAIL_TYPES, AUDIO_TYPES]
+SUPPORTED_TYPES = Literal[EXCEL_TYPES, WORD_TYPES, PPT_TYPES, PDF_TYPES, IMAGE_TYPES, TEXT_TYPES, HTML_TYPES, EMAIL_TYPES, AUDIO_TYPES]
