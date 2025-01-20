82ff5ebe7d2808bc8b4096cd99c16e1cf6162fe5
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-01-20 11:06:42 +0100

    update image rotator

--- clients/python/uiform/types/modalities.py
+++ clients/python/uiform/types/modalities.py
@@ -3,7 +3,7 @@ from typing import Literal
 
 BaseModality = Literal["text", "image", "audio", "video"]
 Modality = Literal[BaseModality, "native"]
-TYPE_FAMILIES = Literal["excel", "word", "powerpoint", "pdf", "image", "text", "email", "audio", "html"]
+TYPE_FAMILIES = Literal["excel", "word", "powerpoint", "pdf", "image", "text", "email", "audio", "html", "web"]
 NativeModalities: dict[TYPE_FAMILIES, Modality] = {
     'excel': 'image',
     'word': 'image',
@@ -11,6 +11,7 @@ NativeModalities: dict[TYPE_FAMILIES, Modality] = {
     'powerpoint': 'image',
     'pdf': 'image',
     'image': 'image',
+    'web': 'image',
     'text': 'text',
     'email': 'native',
     'audio': 'audio'
@@ -23,6 +24,7 @@ PDF_TYPES = Literal[".pdf"]
 IMAGE_TYPES = Literal[".jpg", ".jpeg", ".png", ".gif", ".bmp", ".tiff", ".webp"]
 TEXT_TYPES = Literal[".txt", ".csv", ".tsv", ".md", ".log", ".xml", ".json", ".yaml", ".yml", ".rtf", ".ini", ".conf", ".cfg", ".nfo", ".srt", ".sql", ".sh", ".bat", ".ps1", ".js", ".jsx", ".ts", ".tsx", ".py", ".java", ".c", ".cpp", ".cs", ".rb", ".php", ".swift", ".kt", ".go", ".rs", ".pl", ".r", ".m", ".scala"]
 HTML_TYPES = Literal[".html", ".htm"]
-EMAIL_TYPES = Literal[".eml", ".msg", ".mhtml"]
+WEB_TYPES = Literal[".mhtml"]
+EMAIL_TYPES = Literal[".eml", ".msg"]
 AUDIO_TYPES = Literal[".mp3", ".mp4", ".mpeg", ".mpga", ".m4a", ".wav", ".webm"]
-SUPPORTED_TYPES = Literal[EXCEL_TYPES, WORD_TYPES, PPT_TYPES, PDF_TYPES, IMAGE_TYPES, TEXT_TYPES, HTML_TYPES, EMAIL_TYPES, AUDIO_TYPES]
+SUPPORTED_TYPES = Literal[EXCEL_TYPES, WORD_TYPES, PPT_TYPES, PDF_TYPES, IMAGE_TYPES, TEXT_TYPES, HTML_TYPES, WEB_TYPES, EMAIL_TYPES, AUDIO_TYPES]
