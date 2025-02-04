293d0d51906f59f284daa937c9ab810702407c4c
Luiz Pinheiro <luizjosebp@gmail.com>
2025-02-04 13:10:55 +0000

    [fix] change maybe_parse_to_pydantic to ingest a Schema

--- clients/python/uiform/resources/documents/templates/documentai.py
+++ clients/python/uiform/resources/documents/templates/documentai.py
@@ -143,7 +143,7 @@ class DocumentAIs(SyncAPIResource, BaseDocumentAIMixin):
         # Validate DocumentAPIRequest data (raises exception if invalid)
         request = self.prepare_parse(template, document, image_settings, model, temperature, messages, modality, store)
         response = self._client._request("POST", "/v1/documents/extractions", data=request.model_dump(), idempotency_key=idempotency_key)
-        return maybe_parse_to_pydantic(request.model_dump(), DocumentExtractResponse.model_validate(response))
+        return maybe_parse_to_pydantic(request.form_schema, DocumentExtractResponse.model_validate(response))
 
     @as_context_manager
     def stream(
@@ -187,8 +187,8 @@ class DocumentAIs(SyncAPIResource, BaseDocumentAIMixin):
         for chunk_json in self._client._request_stream("POST", "/v1/documents/extractions", data=request.model_dump(), idempotency_key=idempotency_key):
             if not chunk_json:
                 continue
-            yield maybe_parse_to_pydantic(request.model_dump(), DocumentExtractResponse.model_validate(chunk_json), allow_partial=True)
-        yield maybe_parse_to_pydantic(request.model_dump(), DocumentExtractResponse.model_validate(chunk_json))
+            yield maybe_parse_to_pydantic(request.form_schema, DocumentExtractResponse.model_validate(chunk_json), allow_partial=True)
+        yield maybe_parse_to_pydantic(request.form_schema, DocumentExtractResponse.model_validate(chunk_json))
 
 
 class AsyncDocumentAIs(AsyncAPIResource, BaseDocumentAIMixin):
@@ -221,7 +221,7 @@ class AsyncDocumentAIs(AsyncAPIResource, BaseDocumentAIMixin):
         """
         request = self.prepare_parse(template, document, image_settings, model, temperature, messages, modality, store)
         response = await self._client._request("POST", "/v1/documents/extractions", data=request.model_dump(), idempotency_key=idempotency_key)
-        return maybe_parse_to_pydantic(request.model_dump(), DocumentExtractResponse.model_validate(response))
+        return maybe_parse_to_pydantic(request.form_schema, DocumentExtractResponse.model_validate(response))
 
     @as_async_context_manager
     async def stream(
@@ -262,6 +262,6 @@ class AsyncDocumentAIs(AsyncAPIResource, BaseDocumentAIMixin):
             if not chunk_json:
                 continue
             
-            yield maybe_parse_to_pydantic(request.model_dump(), DocumentExtractResponse.model_validate(chunk_json), allow_partial=True)
+            yield maybe_parse_to_pydantic(request.form_schema, DocumentExtractResponse.model_validate(chunk_json), allow_partial=True)
         # Last chunk with full parsed response
-        yield maybe_parse_to_pydantic(request.model_dump(), DocumentExtractResponse.model_validate(chunk_json))  
+        yield maybe_parse_to_pydantic(request.form_schema, DocumentExtractResponse.model_validate(chunk_json))  
