d3aec036befaa8e3e2146628d5d060bf41e50fca
Luiz Pinheiro <luizjosebp@gmail.com>
2025-02-04 17:32:07 +0000

    [feat] test links webhook handler

--- clients/python/tests/test_automations_links.py
+++ clients/python/tests/test_automations_links.py
@@ -1,14 +1,18 @@
 
 import pytest
+import httpx
+import base64
+import uuid
 from pydantic import HttpUrl
 from typing import Any
 from uiform import UiForm
 
 @pytest.mark.asyncio
-async def test_mailboxes_crud(sync_client: UiForm, company_json_schema: dict[str, Any]) -> None:
-    name = "invoices15"
+async def test_mailboxes_crud(sync_client: UiForm, company_json_schema: dict[str, Any], booking_confirmation_file_path: str) -> None:
+    name = str(uuid.uuid4())
+    print("name", name)
     model = "gpt-4o-mini"
-    webhook_url = HttpUrl('http://localhost:4000/product')
+    webhook_url = HttpUrl('http://localhost:4000/v1/test_ingest_completion')
     # Create
     link = sync_client.automations.links.create(
         name, 
@@ -23,8 +27,28 @@ async def test_mailboxes_crud(sync_client: UiForm, company_json_schema: dict[str
     link = sync_client.automations.links.get(link_id)
     assert link.name == name
     # Update
-    link = sync_client.automations.links.update(link_id, webhook_url=HttpUrl('http://localhost:4000/product2'))
-    assert link.webhook_url == HttpUrl('http://localhost:4000/product2')
+    link = sync_client.automations.links.update(link_id, password="password")
+    assert link.password == "password"
+    # Open and read the booking confirmation file
+
+    # Let's upload a file
+    with open(booking_confirmation_file_path, "rb") as f:
+        async with httpx.AsyncClient(timeout=240) as client:
+            usr_pwd_enc = base64.b64encode(f"{name}:password".encode("utf-8")).decode("utf-8")
+            headers = {
+                "Authorization": f"Basic {usr_pwd_enc}",
+            }
+            files = {
+                "file": f
+            }
+            response = await client.post(
+                sync_client.base_url + f"/v1/automations/links/parse/{link_id}",
+                files=files,
+                headers=headers
+            )
+            assert response.status_code == 200
+
+
     # Delete
     sync_client.automations.links.delete(link_id)
     with pytest.raises(Exception):
