dc616a831531a4d7326c1eca36948471686df2d0
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-02-06 10:28:03 +0100

    fix

--- clients/python/uiform/types/automations/cron.py
+++ clients/python/uiform/types/automations/cron.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel, Field
-from typing import Optional
+from typing import Optional, Literal, Any
+import uuid
+import datetime
+from pydantic import HttpUrl
+from ..image_settings import ImageSettings
+from ..modalities import Modality
+
 
 class CronSchedule(BaseModel):
     second: Optional[int] = Field(0, ge=0, le=59, description="Second (0-59), defaults to 0")
@@ -16,10 +22,27 @@ class CronSchedule(BaseModel):
 
 
 
-class ScrappingConfig(AutomationConfig):
+class ScrappingConfig(BaseModel):
     object: Literal['scrapping_cron'] = "scrapping_cron"
     id: str = Field(default_factory=lambda: "scrapping_" + str(uuid.uuid4()), description="Unique identifier for the scrapping job")
     
     # Scrapping Specific Config
     link: HttpUrl = Field(..., description="Link to be scrapped")
     schedule: CronSchedule
+
+
+    updated_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc))
+
+    # HTTP Config
+    webhook_url: HttpUrl = Field(..., description = "Url of the webhook to send the data to")
+    webhook_headers: dict[str, str] = Field(default_factory=dict, description = "Headers to send with the request")
+
+    modality: Modality
+    image_settings : ImageSettings = Field(default_factory=ImageSettings, description="Preprocessing operations applied to image before sending them to the llm")
+
+    # New attributes
+    model: str = Field(..., description="Model used for chat completion")
+    json_schema: dict[str, Any] = Field(..., description="JSON schema format used to validate the output data.")
+    temperature: float = Field(default=0.0, description="Temperature for sampling. If not provided, the default temperature for the model will be used.", examples=[0.0])
+
+    
