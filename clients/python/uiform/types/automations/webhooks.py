714bc50b858bdc8fccc9d1565600dc8cfd306382
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-02-16 13:12:47 +0100

    New release

--- clients/python/uiform/types/automations/webhooks.py
+++ clients/python/uiform/types/automations/webhooks.py
@@ -1,10 +1,18 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, Any
 from openai.types.chat.parsed_chat_completion import ParsedChatCompletion
-from ..mime import MIMEData
+from ..mime import MIMEData, BaseMIMEData
 from pydantic import EmailStr
 
 class WebhookRequest(BaseModel):
     completion: ParsedChatCompletion
     user: Optional[EmailStr] = None
     file_payload: MIMEData
+    metadata: dict[str, Any]
+
+
+class BaseWebhookRequest(BaseModel):
+    completion: ParsedChatCompletion
+    user: Optional[EmailStr] = None
+    file_payload: BaseMIMEData
+    metadata: dict[str, Any]
