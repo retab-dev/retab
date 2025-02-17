005eab5c2e5a08ed1e214a8d077aafb41d8c95b9
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-02-17 11:52:35 +0100

    Fixed WebhookRequest Data structure

--- clients/python/uiform/types/automations/webhooks.py
+++ clients/python/uiform/types/automations/webhooks.py
@@ -1,18 +1,19 @@
 from pydantic import BaseModel
 from typing import Optional, Any
-from openai.types.chat.parsed_chat_completion import ParsedChatCompletion
 from ..mime import MIMEData, BaseMIMEData
+from uiform.types.documents.extractions import UiParsedChatCompletion
 from pydantic import EmailStr
 
+
 class WebhookRequest(BaseModel):
-    completion: ParsedChatCompletion
+    completion: UiParsedChatCompletion
     user: Optional[EmailStr] = None
     file_payload: MIMEData
     metadata: dict[str, Any]
 
 
 class BaseWebhookRequest(BaseModel):
-    completion: ParsedChatCompletion
+    completion: UiParsedChatCompletion
     user: Optional[EmailStr] = None
     file_payload: BaseMIMEData
     metadata: dict[str, Any]
