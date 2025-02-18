09814f865f6989a649f49f6dcc637078a4d433c1
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-02-18 10:54:15 +0100

    wip log

--- clients/python/uiform/types/events.py
+++ clients/python/uiform/types/events.py
@@ -5,9 +5,16 @@ import datetime
 
 
 
-metadata_key = Literal['user', 'organization', 'link', 'mailbox', 'cron', 'outlook', 'extraction']
+metadata_key = Literal['user', 'organization', 'link', 'mailbox', 'cron', 'outlook', 'extraction', 'webhook']
 
-event_type = Literal['extraction.created']
+event_type = Literal['extraction.created', 
+                     'link.created', 'link.updated', 'link.deleted',
+                     'mailbox.created', 'mailbox.updated', 'mailbox.deleted',
+                     'cron.created', 'cron.updated', 'cron.deleted',
+                     'outlook.created', 'outlook.updated', 'outlook.deleted',
+                     'user.created', 'user.updated', 'user.deleted',
+                     'organization.created', 'organization.updated', 'organization.deleted',
+                     ]
 
 class Event(BaseModel):
     object: Literal['event'] = "event"
@@ -16,3 +23,7 @@ class Event(BaseModel):
     created_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc))
     data: dict[str, Any] = Field(..., description="Event payload. Payloads match the corresponding API objects.")
     metadata: Optional[dict[metadata_key, str]] = Field(default=None, description="Ids giving informations about the event. Ex: user.created.metadata = {'user': 'usr_8478973619047837'}")
+
+
+class StoredEvent(Event):
+    organization_id: str = Field(..., description="Organization ID")
