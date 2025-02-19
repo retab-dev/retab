136a8a32d8ff65c2508880257e418bd9e632709f
Luiz Pinheiro <luizjosebp@gmail.com>
2025-02-19 09:53:50 +0000

    [feat] add limit

--- clients/python/uiform/types/jobs/webcrawl.py
+++ clients/python/uiform/types/jobs/webcrawl.py
@@ -5,6 +5,7 @@ CheckPoint = Literal[None, "webcrawl_started", "webcrawl_completed", "webcrawl_f
 
 class WebcrawlInputData(BaseModel):
     url: str
+    limit: int = Field(default=3, ge=1, le=100)
 
 class WebcrawlJob(BaseModel):
     job_type: Literal["webcrawl"] = "webcrawl"
