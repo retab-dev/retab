a7ce153a052b8622548adad9d711192e378fb4ff
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-02-20 12:22:51 +0100

    frontend more mature - backup before returning to the layout

--- clients/python/uiform/types/automations/endpoint.py
+++ clients/python/uiform/types/automations/endpoint.py
@@ -7,7 +7,11 @@ from ..modalities import Modality
 from pydantic import HttpUrl
 from ..pagination import ListMetadata
 
-
+from ..._utils.json_schema import clean_schema
+from ..._utils.mime import generate_sha_hash_from_string
+import copy
+import json
+from pydantic import computed_field
 
 class Endpoint(BaseModel):
     object: Literal['automation.endpoint'] = "automation.endpoint"
@@ -26,6 +30,34 @@ class Endpoint(BaseModel):
     model: str = Field(..., description="Model used for chat completion")
     json_schema: dict[str, Any] = Field(..., description="JSON schema format used to validate the output data.")
     temperature: float = Field(default=0.0, description="Temperature for sampling. If not provided, the default temperature for the model will be used.", examples=[0.0])
+
+
+        
+    @computed_field   # type: ignore
+    @property
+    def schema_data_id(self) -> str:
+        """Returns the SHA1 hash of the schema data, ignoring all prompt/description/default fields.
+        
+        Returns:
+            str: A SHA1 hash string representing the schema data version.
+        """
+        return "sch_data_id_" + generate_sha_hash_from_string(
+            json.dumps(
+                clean_schema(copy.deepcopy(self.json_schema), remove_custom_fields=True, fields_to_remove=["description", "default", "title", "required", "examples", "deprecated", "readOnly", "writeOnly"]),
+                sort_keys=True).strip(), 
+            "sha1")
+
+    # This is a computed field, it is exposed when serializing the object
+    @computed_field   # type: ignore
+    @property
+    def schema_id(self) -> str:
+        """Returns the SHA1 hash of the complete schema.
+        
+        Returns:
+            str: A SHA1 hash string representing the complete schema version.
+        """
+        return "sch_id_" + generate_sha_hash_from_string(json.dumps(self.json_schema, sort_keys=True).strip(), "sha1")
+
 
 
     @field_serializer('webhook_url')
