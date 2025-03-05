d55a7ee00c8243ade77e9dc6165a4261924404af
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-03-05 16:26:53 +0100

    endpoints are back

---
+++ clients/python/uiform/types/automations/endpoints.py
@@ -0,0 +1,105 @@
+from pydantic import BaseModel, Field, field_serializer
+from typing import Literal, Any, Optional
+import datetime
+from ..image_settings import ImageSettings
+from ..modalities import Modality
+from pydantic import HttpUrl
+from ..pagination import ListMetadata
+
+from ..._utils.json_schema import clean_schema
+import copy
+import json
+from pydantic import computed_field
+import nanoid # type: ignore
+from ..._utils.mime import generate_blake2b_hash_from_string
+from ..logs import AutomationConfig
+
+
+class Endpoint(AutomationConfig):
+    object: Literal['automation.endpoint'] = "automation.endpoint"
+    id: str = Field(default_factory=lambda: "endp_" + nanoid.generate(), description="Unique identifier for the extraction endpoint")
+    
+    # Extraction Endpoint Specific Config
+    name: str = Field(..., description="Name of the extraction endpoint")
+
+    # Automation Config
+    webhook_url: HttpUrl = Field(..., description = "Url of the webhook to send the data to")
+    webhook_headers: dict[str, str] = Field(default_factory=dict, description = "Headers to send with the request")
+    updated_at: datetime.datetime = Field(default_factory=lambda: datetime.datetime.now(datetime.timezone.utc))
+    # DocumentExtraction Config
+    modality: Modality
+    image_settings : ImageSettings = Field(default_factory=ImageSettings, description="Preprocessing operations applied to image before sending them to the llm")
+    model: str = Field(..., description="Model used for chat completion")
+    json_schema: dict[str, Any] = Field(..., description="JSON schema format used to validate the output data.")
+    temperature: float = Field(default=0.0, description="Temperature for sampling. If not provided, the default temperature for the model will be used.", examples=[0.0])
+
+
+    @computed_field   # type: ignore
+    @property
+    def schema_data_id(self) -> str:
+        """Returns the SHA1 hash of the schema data, ignoring all prompt/description/default fields.
+        
+        Returns:
+            str: A SHA1 hash string representing the schema data version.
+        """
+        return "sch_data_id_" + generate_blake2b_hash_from_string(
+            json.dumps(
+                clean_schema(copy.deepcopy(self.json_schema), remove_custom_fields=True, fields_to_remove=["description", "default", "title", "required", "examples", "deprecated", "readOnly", "writeOnly"]),
+                sort_keys=True).strip() 
+            )
+
+    # This is a computed field, it is exposed when serializing the object
+    @computed_field   # type: ignore
+    @property
+    def schema_id(self) -> str:
+        """Returns the SHA1 hash of the complete schema.
+        
+        Returns:
+            str: A SHA1 hash string representing the complete schema version.
+        """
+        return "sch_id_" + generate_blake2b_hash_from_string(json.dumps(self.json_schema, sort_keys=True).strip())
+
+
+
+    @field_serializer('webhook_url')
+    def url2str(self, val: HttpUrl) -> str:
+        return str(val)
+
+
+class ListEndpoints(BaseModel):
+    data: list[Endpoint]
+    list_metadata: ListMetadata
+
+
+
+class UpdateEndpointRequest(BaseModel):
+    id: str
+
+    # ------------------------------
+    # Endpoint Config
+    # ------------------------------
+    name: Optional[str] = None
+
+    # ------------------------------
+    # HTTP Config
+    # ------------------------------
+    webhook_url: Optional[HttpUrl] = None
+    webhook_headers: Optional[dict[str, str]] = None
+
+    # ------------------------------
+    # DocumentExtraction Parameters
+    # ------------------------------
+    # DocumentProcessing Parameters
+    image_settings: Optional[ImageSettings] = None
+    modality: Optional[Modality] = None
+    # Others DocumentExtraction Parameters
+    model: Optional[str] = None
+    temperature: Optional[float] = None
+    json_schema: Optional[dict[str, Any]] = None
+
+    @field_serializer('webhook_url')
+    def url2str(self, val: HttpUrl | None) -> str | None:
+        if isinstance(val, HttpUrl):
+            return str(val)
+        return val
+
