a5feed18602ee7f2eb440c5b42de3f1c39b20139
Luiz Pinheiro <luizjosebp@gmail.com>
2025-03-20 10:35:03 +0000

    [feat] n_consensus field + store = True par default

--- clients/python/uiform/types/documents/extractions.py
+++ clients/python/uiform/types/documents/extractions.py
@@ -27,10 +27,11 @@ class DocumentExtractRequest(BaseModel):
     json_schema: dict[str, Any] = Field(..., description="JSON schema format used to validate the output data.")
     temperature: float = Field(default=0.0, description="Temperature for sampling. If not provided, the default temperature for the model will be used.", examples=[0.0])
     reasoning_effort: ChatCompletionReasoningEffort = Field(default="medium", description="The effort level for the model to reason about the input data. If not provided, the default reasoning effort for the model will be used.")
+    n_consensus: int = Field(default=1, description="Number of consensus models to use for extraction. If greater than 1 the temperature cannot be 0.")
     # Regular fields
     stream: bool = Field(default=False, description="If true, the extraction will be streamed to the user using the active WebSocket connection")
     seed: int | None = Field(default=None, description="Seed for the random number generator. If not provided, a random seed will be generated.", examples=[None])
-    store: bool = Field(default=False, description="If true, the extraction will be stored in the database")
+    store: bool = Field(default=True, description="If true, the extraction will be stored in the database")
     need_validation: bool = Field(default=False, description="If true, the extraction will be validated against the schema")
     @property
     def provider(self) -> AIProvider:
