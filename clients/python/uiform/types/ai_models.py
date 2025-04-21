b8cf9484ec864c27b7790c53e3e2bf1a39564da3
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-04-21 17:05:42 +0200

    Added gemini 2.5 flash

--- clients/python/uiform/types/ai_models.py
+++ clients/python/uiform/types/ai_models.py
@@ -3,10 +3,10 @@ from typing import Literal
 AIProvider = Literal["OpenAI", "Gemini"]#, "Anthropic", "xAI"]
 OpenAICompatibleProvider = Literal["OpenAI", "Gemini"]#, "xAI"]
 GeminiModel = Literal[
+    "gemini-2.5-flash-preview-04-17",
     "gemini-2.5-pro-exp-03-25",
     "gemini-2.0-flash",
     "gemini-2.0-flash-lite",
-    "gemini-1.5-pro",
 ]
 AnthropicModel = Literal["claude-3-5-sonnet-latest","claude-3-5-sonnet-20241022",
                          "claude-3-5-haiku-20241022",
