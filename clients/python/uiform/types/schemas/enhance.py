fe38c3eb332c9598ea3ae90d36483caeec895bc1
Luiz Pinheiro <luizjosebp@gmail.com>
2025-06-04 10:28:30 +0000

    [feat] update on EnhanceSchemaConfig

--- clients/python/uiform/types/schemas/enhance.py
+++ clients/python/uiform/types/schemas/enhance.py
@@ -1,4 +1,4 @@
-from typing import Any, Literal, Self, TypedDict, Literal
+from typing import Any, Self, TypedDict, Literal
 from openai.types.chat.chat_completion_reasoning_effort import ChatCompletionReasoningEffort
 from pydantic import BaseModel, Field, model_validator
 
@@ -7,32 +7,23 @@ from ..modalities import Modality
 
 
 class EnhanceSchemaConfig(BaseModel):
-    allow_fields_insertion: bool = False  # Whether to allow the llm to insert new fields
-    allow_fields_deletion: bool = False  # Whether to allow the llm to delete existing fields
     allow_field_description_update: bool = False  # Whether to allow the llm to update the description of existing fields
-    allow_reasoning_field_toggle: bool = False  # Whether to allow the llm to toggle the reasoning for fields
     allow_system_prompt_update: bool = True  # Whether to allow the llm to update the system prompt
+    allow_reasoning_field_toggle: bool = False  # Whether to allow the llm to toggle the reasoning for fields
 
     # Model validator
     @model_validator(mode="after")
     def check_at_least_one_tool_allowed(self) -> Self:
-        if not any(
-            [self.allow_fields_insertion, self.allow_fields_deletion, self.allow_field_description_update, self.allow_reasoning_field_toggle, self.allow_system_prompt_update]
-        ):
+        if not any([self.allow_field_description_update, self.allow_system_prompt_update, self.allow_reasoning_field_toggle]):
             raise ValueError("At least one tool must be allowed")
-        # There are some not implemented tools
-        if any([self.allow_fields_insertion, self.allow_fields_deletion, self.allow_reasoning_field_toggle]):
-            raise ValueError("Fields insertion, Field deletion, Field description update and Reasoning field toggle are not implemented yet, sorry about that!")
         return self
 
 
 # Define a typed Dict for EnhanceSchemaConfig (for now it is kind static, but we will add more flexibility in the future)
 class EnhanceSchemaConfigDict(TypedDict, total=False):
-    allow_fields_insertion: Literal[False]
-    allow_fields_deletion: Literal[False]
-    allow_reasoning_field_toggle: Literal[False]
     allow_field_description_update: bool
     allow_system_prompt_update: bool
+    allow_reasoning_field_toggle: bool
 
 
 class EnhanceSchemaRequest(BaseModel):
