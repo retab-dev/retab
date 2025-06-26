7b8c1d2e410c4738b0c275a1468b5b30bd46a855
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-06-26 10:39:21 +0000

    cleanup

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -1,29 +1,29 @@
 from setuptools import find_packages, setup  # type: ignore
 
 # Read requirements.txt and use it for the install_requires parameter
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     requirements_list = f.read().splitlines()
 
 setup(
-    name='retab',
-    version='0.0.38',
-    author='Retab',
-    author_email='contact@retab.com',
-    description='Retab official python library',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/Retab-dev/retab',
-    project_urls={'Team website': 'https://retab.com'},
+    name="retab",
+    version="0.0.39",
+    author="Retab",
+    author_email="contact@retab.com",
+    description="Retab official python library",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Retab-dev/retab",
+    project_urls={"Team website": "https://retab.com"},
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS',
-        'Intended Audience :: Science/Research',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Intended Audience :: Science/Research",
     ],
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     install_requires=requirements_list,
     include_package_data=True,
-    package_data={'retab': ["**/*.yaml"]},
+    package_data={"retab": ["**/*.yaml"]},
 )
