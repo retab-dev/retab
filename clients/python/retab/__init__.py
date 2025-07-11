c8d446b7f3716a995a59de4e620c28b1a82a46bc
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-07-11 15:00:56 +0000

    surfaced some items

--- clients/python/retab/__init__.py
+++ clients/python/retab/__init__.py
@@ -1,4 +1,5 @@
 from .client import AsyncRetab, Retab
 from .types.schemas.object import Schema
+from . import utils
 
-__all__ = ["Retab", "AsyncRetab", "Schema"]
+__all__ = ["Retab", "AsyncRetab", "Schema", "utils"]
