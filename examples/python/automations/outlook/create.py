b857d8e77b65e61af35170fa91588165ac5ce64e
Luiz Pinheiro <luizjosebp@gmail.com>
2025-07-18 14:47:03 +0000

    [feat] parser arguments for outlook automation example

--- examples/python/automations/outlook/create.py
+++ examples/python/automations/outlook/create.py
@@ -1,14 +1,33 @@
+import os
+import argparse
 from dotenv import load_dotenv
 from retab import Retab
 
-assert load_dotenv("../../../.env.production")
+# File Path
+file_path = os.path.dirname(os.path.abspath(__file__))
 
-reclient = Retab()
 
-automation = reclient.processors.automations.outlook.create(
-    name="Outlook Automation",
-    processor_id="proc_o4dtLxizT0kDAjeKuyVLA",
-    webhook_url="https://your-server.com/webhook",
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Create an Outlook automation")
+    parser.add_argument("--env", required=True, choices=["dev", "staging", "prod"],
+                       default="local",
+                       help="Source environment")
+    parser.add_argument("--processor-id", required=True,
+                       help="ID of processor to create automation for")
+    parser.add_argument("--webhook-url", required=False,
+                       help="Webhook URL to send automation results to",
+                       default="https://your-server.com/webhook")
+    
+    args = parser.parse_args()
+    
+    assert load_dotenv(f"{file_path}/../../../.env.{args.env}")
 
-print(automation.model_dump_json(indent=2))
+    reclient = Retab()
+
+    automation = reclient.processors.automations.outlook.create(
+        name="Outlook Automation",
+        processor_id=args.processor_id,
+        webhook_url=args.webhook_url,
+    )
+
+    print(automation.model_dump_json(indent=2))
