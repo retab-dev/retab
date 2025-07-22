24598f9779af09032148689985a023096e502d05
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-07-22 13:21:56 +0000

    Update snippets

---
+++ script.py
@@ -0,0 +1,10 @@
+from retab import Retab
+from dotenv import load_dotenv
+
+load_dotenv(".env.local")
+
+client = Retab()
+
+extraction = client.documents.extract(json_schema="assets/code/invoice_schema.json", document="assets/code/invoice.jpeg", modality="native", model="gpt-4.1-nano", temperature=0)
+
+print("Extraction: ", extraction.model_dump_json(indent=4))
