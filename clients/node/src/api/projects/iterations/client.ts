4c99a6b009dc2b079194c93f47d151c70e16a1c7
epic-pigeon <43731966+epic-pigeon@users.noreply.github.com>
2025-07-28 12:24:26 +0200

    done

--- clients/node/src/api/projects/iterations/client.ts
+++ clients/node/src/api/projects/iterations/client.ts
@@ -1,37 +1,37 @@
 import { CompositionClient } from "@/client";
 import * as z from "zod";
-import { ZInferenceSettings, Iteration, ZIteration, ZPatchIterationRequest, dataArray } from "@/types";
+import { ZInferenceSettings, ZPatchIterationRequest, dataArray, ZIterationsIteration, IterationsIteration } from "@/types";
 
 export default class APIProjectsIterations extends CompositionClient {
     constructor(client: CompositionClient) {
         super(client);
     }
-    async create(projectId: string, body: z.input<typeof ZInferenceSettings>): Promise<Iteration> {
-        return this._fetchJson(ZIteration, {
+    async create(projectId: string, body: z.input<typeof ZInferenceSettings>): Promise<IterationsIteration> {
+        return this._fetchJson(ZIterationsIteration, {
             url: `/v1/projects/${projectId}/iterations`,
             method: "POST",
             body: await ZInferenceSettings.parseAsync(body),
         });
     }
 
-    async update(projectId: string, iterationId: string, body: z.input<typeof ZPatchIterationRequest>): Promise<Iteration> {
-        return this._fetchJson(ZIteration, {
+    async update(projectId: string, iterationId: string, body: z.input<typeof ZPatchIterationRequest>): Promise<IterationsIteration> {
+        return this._fetchJson(ZIterationsIteration, {
             url: `/v1/projects/${projectId}/iterations/${iterationId}`,
             method: "PATCH",
             body: await ZPatchIterationRequest.parseAsync(body),
         });
     }
 
-    async list(projectId: string, params?: {model?: string}): Promise<Iteration[]> {
-        return this._fetchJson(dataArray(ZIteration), {
+    async list(projectId: string, params?: {model?: string}): Promise<IterationsIteration[]> {
+        return this._fetchJson(dataArray(ZIterationsIteration), {
             url: `/v1/projects/${projectId}/iterations`,
             method: "GET",
             params: params,
         });
     }
 
-    async get(projectId: string, iterationId: string): Promise<Iteration> {
-        return this._fetchJson(ZIteration, {
+    async get(projectId: string, iterationId: string): Promise<IterationsIteration> {
+        return this._fetchJson(ZIterationsIteration, {
             url: `/v1/projects/${projectId}/iterations/${iterationId}`,
             method: "GET",
         });
