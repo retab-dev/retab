3c0781fe722a15546aaa3c6c34c5917a177c5104
Victor Soto <sotovictor@outlook.fr>
2025-08-01 11:19:45 +0200

    [updates]-small updates cookbook

--- script.py
+++ script.py
@@ -7,4 +7,4 @@ client = Retab()
 
 extraction = client.documents.extract(json_schema="assets/code/invoice_schema.json", document="assets/code/invoice.jpeg", modality="native", model="gpt-4.1-nano", temperature=0)
 
-print("Extraction: ", extraction.model_dump_json(indent=4))
+print("Extraction: ", extraction.model_dump_json(indent=2))
