b2b530ce713c691f52c0944377d4abe4e14e6fbd
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-08-17 08:51:03 +0000

    updated node client

--- clients/node/src/api/client.ts
+++ clients/node/src/api/client.ts
@@ -2,17 +2,14 @@ import { AbstractClient, CompositionClient } from "../client.js";
 import APIModels from "./models/client";
 import APIDocuments from "./documents/client";
 import APISchemas from "./schemas/client";
-import APIDeployments from "./deployments/client";
 import APIProjects from "./projects/client";
 
 export default class APIV1 extends CompositionClient {
     constructor(client: AbstractClient) {
         super(client);
     }
-
     models = new APIModels(this);
     documents = new APIDocuments(this);
     schemas = new APISchemas(this);
-    deployments = new APIDeployments(this);
     projects = new APIProjects(this);
 }
