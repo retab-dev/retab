b17337ce015423d905745af650e944bd939ffef8
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-08-28 15:48:12 +0000

    New python sdk version

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="retab",
-    version="0.0.59",
+    version="0.0.60",
     author="Retab",
     author_email="contact@retab.com",
     description="Retab official python library",
