2f40c28d9d4f6f0db58f73a0d164485a62620396
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-09-11 10:23:03 +0000

    updated sdk

--- cookbook/python/documents/create_messages.py
+++ cookbook/python/documents/create_messages.py
@@ -1,35 +1,25 @@
 import os
 from dotenv import load_dotenv
 from retab import Retab
-from openai import OpenAI
-
 
 # ---------------------------------------------
-## Quick example: Summarize a document using Retab + OpenAI, without a schema.
+## Quick example: Parse a document in markdown format using Retab.
 # ---------------------------------------------
 
 # Load environment variables
 load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
 retab_api_key = os.getenv("RETAB_API_KEY")
 
-assert api_key, "Missing OPENAI_API_KEY"
 assert retab_api_key, "Missing RETAB_API_KEY"
 
 # Retab Setup
-reclient = Retab(api_key=retab_api_key)
-doc_msg = reclient.documents.create_messages(
+client = Retab(api_key=retab_api_key)
+result = client.documents.parse(
     document="../../assets/docs/booking_confirmation.jpg",
-    modality="native",
-    image_resolution_dpi=96,
+    model="gemini-2.5-flash",
+    image_resolution_dpi=192,
     browser_canvas="A4",
 )
 
-
-client = OpenAI(api_key=api_key)
-completion = client.chat.completions.create(model="gpt-4o-mini", messages=doc_msg.openai_messages + [{"role": "user", "content": "Summarize the document"}])
-
-# Output
-print("\n📝 OpenAI Completion Output:")
-print(completion.model_dump_json(indent=2))
+print(result.pages)
