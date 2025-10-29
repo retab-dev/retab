7ba37a2782c4f97ecb73d69fc19bb9fd65ca9126
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2025-10-29 16:15:17 +0000

    cleanup sdk

--- clients/python/retab/resources/projects/client.py
+++ clients/python/retab/resources/projects/client.py
@@ -10,21 +10,16 @@ from ...utils.mime import MIMEData, prepare_mime_document
 from ...types.documents.extract import RetabParsedChatCompletion
 from ...types.projects import Project, PatchProjectRequest, BaseProject
 from ...types.standards import PreparedRequest, DeleteResponse, FieldUnset
-from .documents import Documents, AsyncDocuments
-from .iterations import Iterations, AsyncIterations
-
 
 class ProjectsMixin:
     def prepare_create(
         self,
         name: str,
-        json_schema: dict[str, Any],
         **extra_body: Any,
     ) -> PreparedRequest:
         # Use BaseProject model
         eval_dict: dict[str, Any] = {
             "name": name,
-            "json_schema": json_schema,
         }
         if extra_body:
             eval_dict.update(extra_body)
@@ -160,13 +155,10 @@ class Projects(SyncAPIResource, ProjectsMixin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.documents = Documents(self._client)
-        self.iterations = Iterations(self._client)
 
     def create(
         self,
         name: str,
-        json_schema: dict[str, Any],
         **extra_body: Any,
     ) -> Project:
         """
@@ -174,15 +166,13 @@ class Projects(SyncAPIResource, ProjectsMixin):
 
         Args:
             name: The name of the project
-            json_schema: The JSON schema for the project
-            documents: The documents to associate with the project
 
         Returns:
             Project: The created project
         Raises:
             HTTPException if the request fails
         """
-        request = self.prepare_create(name, json_schema, **extra_body)
+        request = self.prepare_create(name, **extra_body)
         response = self._client._prepared_request(request)
         return Project(**response)
 
@@ -199,37 +189,6 @@ class Projects(SyncAPIResource, ProjectsMixin):
             HTTPException if the request fails
         """
         request = self.prepare_get(project_id)
-        response = self._client._prepared_request(request)
-        return Project(**response)
-
-    def update(
-        self,
-        project_id: str,
-        name: str = FieldUnset,
-        json_schema: dict[str, Any] = FieldUnset,
-        **extra_body: Any,
-    ) -> Project:
-        """
-        Update an project with partial updates.
-
-        Args:
-            project_id: The ID of the project to update
-            name: Optional new name for the project
-            json_schema: Optional new JSON schema
-            documents: Optional list of documents to update
-            iterations: Optional list of iterations to update
-
-        Returns:
-            Project: The updated project
-        Raises:
-            HTTPException if the request fails
-        """
-        request = self.prepare_update(
-            project_id=project_id,
-            name=name,
-            json_schema=json_schema,
-            **extra_body,
-        )
         response = self._client._prepared_request(request)
         return Project(**response)
 
@@ -314,23 +273,20 @@ class AsyncProjects(AsyncAPIResource, ProjectsMixin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.documents = AsyncDocuments(self._client)
-        self.iterations = AsyncIterations(self._client)
 
-    async def create(self, name: str, json_schema: Dict[str, Any]) -> Project:
+    async def create(self, name: str, **extra_body: Any) -> Project:
         """
         Create a new project.
 
         Args:
             name: The name of the project
-            json_schema: The JSON schema for the project
 
         Returns:
             Project: The created project
         Raises:
             HTTPException if the request fails
         """
-        request = self.prepare_create(name, json_schema)
+        request = self.prepare_create(name, **extra_body)
         response = await self._client._prepared_request(request)
         return Project(**response)
 
@@ -350,36 +306,7 @@ class AsyncProjects(AsyncAPIResource, ProjectsMixin):
         response = await self._client._prepared_request(request)
         return Project(**response)
 
-    async def update(
-        self,
-        project_id: str,
-        name: str = FieldUnset,
-        json_schema: dict[str, Any] = FieldUnset,
-    ) -> Project:
-        """
-        Update an project with partial updates.
-
-        Args:
-            id: The ID of the project to update
-            name: Optional new name for the project
-            json_schema: Optional new JSON schema
-            documents: Optional list of documents to update
-            iterations: Optional list of iterations to update
-
-        Returns:
-            Project: The updated project
-        Raises:
-            HTTPException if the request fails
-        """
-        request = self.prepare_update(
-            project_id=project_id,
-            name=name,
-            json_schema=json_schema,
-        )
-        response = await self._client._prepared_request(request)
-        return Project(**response)
-
-    async def list(self) -> List[Project]:
+    async def list(self, **extra_params: Any) -> List[Project]:
         """
         List projects for a project.
 
@@ -388,7 +315,7 @@ class AsyncProjects(AsyncAPIResource, ProjectsMixin):
         Raises:
             HTTPException if the request fails
         """
-        request = self.prepare_list()
+        request = self.prepare_list(**extra_params)
         response = await self._client._prepared_request(request)
         return [Project(**item) for item in response.get("data", [])]
 
@@ -419,6 +346,7 @@ class AsyncProjects(AsyncAPIResource, ProjectsMixin):
         n_consensus: int | None = None,
         seed: int | None = None,
         store: bool = True,
+        **extra_form: Any,
     ) -> RetabParsedChatCompletion:
         """Extract documents from a project.
 
