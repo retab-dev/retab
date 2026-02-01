ee046e2e4b90999927fc8ef9b5f59c0454eb556f
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2026-02-01 11:57:55 +0000

    packages updates

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -6,7 +6,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="retab",
-    version="0.0.91",
+    version="0.0.95",
     author="Retab",
     author_email="contact@retab.com",
     description="Retab official python library",
