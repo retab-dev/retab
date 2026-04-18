c2cf6676e4f623bbcc20f07e0f10a565b01fb459
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2026-04-18 21:25:47 +0200

    update

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -18,7 +18,7 @@ requirements_list = load_requirements()
 
 setup(
     name="retab",
-    version="0.0.121",
+    version="0.0.123",
     author="Retab",
     author_email="contact@retab.com",
     description="Retab official python library",
