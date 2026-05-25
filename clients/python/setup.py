e0adfc28fb56c331a919a8b7246a879cb9f0b36b
Sacha Ichbiah <98276351+sacha-ichbiah@users.noreply.github.com>
2026-05-25 02:15:13 +0200

    update sdk packages

--- clients/python/setup.py
+++ clients/python/setup.py
@@ -18,7 +18,7 @@ requirements_list = load_requirements()
 
 setup(
     name="retab",
-    version="0.0.142",
+    version="0.0.143",
     author="Retab",
     author_email="contact@retab.com",
     description="Retab official python library",
